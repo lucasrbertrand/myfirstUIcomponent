5e3aa994e769670e5959155196d886b04401fe11
unknown <lucas.r.bertrand@gmail.com>
2022-11-01 11:45:08 -0300

    refactored

--- script.js
+++ script.js
@@ -7,22 +7,27 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
-//A for loop to go through the .show-modal class (we got 3 uses of it)
-//Then added I event listener to click the 3 buttons
-//When they are clicked the class 'hidden' is removed showing the box we want to show
-
-for (let i = 0; i < btnShowModal.length; i++)
-  btnShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-
 //Refactored, same code was found two times
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+// For easier understanding
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//A for loop to go through the .show-modal class (we got 3 uses of it)
+//Then added I event listener to click the 3 buttons
+//When they are clicked the class 'hidden' is removed showing the box we want to show
+//Refactored is above this one
+
+for (let i = 0; i < btnShowModal.length; i++)
+  btnShowModal[i].addEventListener('click', openModal)
 
 //Function refactored applied to close Modal when clicked in the X of the box, and in the outside part of the box
 
